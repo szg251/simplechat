36de622af3d9a99ac0a9f156efe362185e5e5eab
Gergo <gege251@gmail.com>
2017-07-10 20:48:38 +0900

    backend added

--- build/logger.js
+++
@@ -1,8 +0,0 @@
-
-module.exports = exports = logger;
-
-function logger(message) {
-  var date =　new Date();
-  var timestamp = '[' + date.getHours() + ':' + date.getMinutes() + ':' + date.getSeconds() + '] ';
-  console.log(timestamp + message);
-}
