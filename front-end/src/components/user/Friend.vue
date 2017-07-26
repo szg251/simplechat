842e03bad3656de403f45e35057c77a8d01193df
Gergo <gege251@gmail.com>
2017-07-26 09:32:11 +0900

    Friend request button to friend page

--- front-end/src/components/user/Friend.vue
+++ front-end/src/components/user/Friend.vue
@@ -5,6 +5,10 @@
     <div><strong>UserId:</strong> {{userId}}</div>
     <div><strong>Fullname:</strong> {{fullname}}</div>
     <div><strong>Introduction:</strong> {{introduction}}</div>
+    <div>
+      <a class="btn btn-default" v-if="!isFriendRequestSent" @click="sendReq">Send friend request</a>
+      <a class="btn btn-default" v-else @click="cancelReq">Cancel friend request</a> 
+    </div>
   </div>
 </template>
 
@@ -19,7 +23,8 @@ export default {
         userId: '',
         fullname: '',
         introduction: '',
-        imageSrc: ''
+        imageSrc: '',
+        isFriendRequestSent: false
       }
   },
   props: ['currentUser', 'friendId'],
@@ -37,6 +42,23 @@ export default {
           this.introduction = result.data.friend.introduction,
           this.imageSrc = result.data.friend.imageSrc
         });
+    },
+    sendReq: function() {
+      axios.put(routes.apiRoutes.sendFriendRequest(this.currentUser), {friendId: this.friendId})
+        .then(result => {
+          if (result.data.success) {
+            this.isFriendRequestSent = true;
+          }
+        });
+      
+    },
+    cancelReq: function () {
+      axios.delete(routes.apiRoutes.cancelFriendRequest(this.currentUser), {data: {friendId: this.friendId}})
+        .then(result => {
+          if (result.data.success) {
+            this.isFriendRequestSent = false;
+          }
+        })
     }
   },
   watch: {
